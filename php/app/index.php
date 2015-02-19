84658b8cbc34e873855b915383e51933b1fd9f27
Jose Padilla <jpadilla1@users.noreply.github.com>
2015-02-19 08:48:08 -0400

    added db schema and propel dependencies

--- php/app/index.php
+++ php/app/index.php
@@ -1,13 +1,30 @@
 <?php
-require 'vendor/autoload.php';
+// setup the autoloading
+// require 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
-$app->get('/books/:id', function ($id) {
-    echo $id;
-});
-$app->run();
+// // setup Propel
+// require_once 'generated-conf/config.php';
 
+// $app = new \Slim\Slim();
+// $app->get('/', function () {
+//     echo "Welcome!";
+// });
+// $app->get('/hello/:name', function ($name) {
+//     echo "Hello, $name";
+// });
+// $app->get('/books/:id', function ($id) {
+//     echo $id;
+// });
+// $app->run();
+
+try{
+    $dbh = new pdo( 'mysql:host=' . getenv('DB_1_PORT_3306_TCP_ADDR') . ':' . getenv('DB_1_PORT_3306_TCP_PORT')  .  ';dbname=salhud',
+                    'root',
+                    'root',
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    die(json_encode(array('outcome' => true)));
+}
+catch(PDOException $ex){
+    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+}
  ?>
---
+++ php/app/propel.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'propel' => [
+        'database' => [
+            'connections' => [
+                'salhud' => [
+                    'adapter'    => 'mysql',
+                    'classname'  => 'Propel\Runtime\Connection\ConnectionWrapper',
+                    'dsn'        => 'mysql:host=' . getenv('DB_1_PORT_3306_TCP_ADDR') . ':' . getenv('DB_1_PORT_3306_TCP_PORT')  .  ';dbname=salhud',
+                    'user'       => 'root',
+                    'password'   => 'root',
+                    'attributes' => []
+                ]
+            ]
+        ],
+        'runtime' => [
+            'defaultConnection' => 'salhud',
+            'connections' => ['salhud']
+        ],
+        'generator' => [
+            'defaultConnection' => 'salhud',
+            'connections' => ['salhud']
+        ]
+    ]
+];
