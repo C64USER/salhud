b35da3e9a3dae71933100cd252041561c49f7653
Jose Padilla <jpadilla1@users.noreply.github.com>
2015-02-23 01:48:01 -0400

    switched everything to use volumes from a data only container

--- php/app/index.php
+++ php/app/index.php
@@ -1,35 +1,30 @@
 <?php
 // setup the autoloading
-// require 'vendor/autoload.php';
+require '/php/vendor/autoload.php';
 
-// // setup Propel
-// require_once 'generated-conf/config.php';
+// setup Propel
+require_once 'generated-conf/config.php';
 
-// $app = new \Slim\Slim();
-// $app->get('/', function () {
-//     echo "Welcome!";
-// });
-// $app->get('/hello/:name', function ($name) {
-//     echo "Hello, $name";
-// });
-// $app->get('/books/:id', function ($id) {
-//     echo $id;
-// });
-// $app->run();
-
-try{
-    $dbh = new pdo( 'mysql:host=' . getenv('DB_1_PORT_3306_TCP_ADDR') . ':'  . getenv('DB_1_PORT_3306_TCP_PORT') . ';dbname=salhud',
-                    'root',
-                    'root',
-                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    $result = $dbh->query("SHOW TABLES");
-
-    while ($row = $result->fetch(PDO::FETCH_NUM)) {
-        echo $row[0]."<br>";
+$app = new \Slim\Slim();
+$app->get('/', function () {
+    $region = new Region();
+    $region->setName('San Juan');
+    $region->setPopulation(123);
+    $region->save();
+    $regions = RegionQuery::create()->find();
+    // $regions contains a collection of Region objects
+    // one object for every row of the author table
+    foreach($regions as $r) {
+      echo $r->getName();
+      echo $r->getPopulation();
     }
-    die(json_encode(array('outcome' => true)));
-}
-catch(PDOException $ex){
-    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
-}
+});
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+$app->get('/books/:id', function ($id) {
+    echo $id;
+});
+$app->run();
+
  ?>
